dec8db06db9f6f433b405b2b902ee979b814b077
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-06 15:20:52 -0700

    Added zones and eventrules microservices

--- checkout.ps1
+++ checkout.ps1
@@ -5,9 +5,13 @@ git clone git@github.com:iquipsys-positron/iqs-services-controlobjects-node
 git clone git@github.com:iquipsys-positron/iqs-services-objectgroups-node
 git clone git@github.com:iquipsys-positron/iqs-services-dataprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-services-deviceprofiles-node
+git clone git@github.com:iquipsys-positron/iqs-services-zones-node
+git clone git@github.com:iquipsys-positron/iqs-services-eventrules-node
 
 git clone git@github.com:iquipsys-positron/iqs-clients-devices-node
 git clone git@github.com:iquipsys-positron/iqs-clients-controlobjects-node
 git clone git@github.com:iquipsys-positron/iqs-clients-objectgroups-node
 git clone git@github.com:iquipsys-positron/iqs-clients-dataprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-clients-deviceprofiles-node
+git clone git@github.com:iquipsys-positron/iqs-clients-zones-node
+git clone git@github.com:iquipsys-positron/iqs-clients-eventrules-node
