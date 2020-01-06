18a3be1e046cb3cb5334d39d4fe0b3540810cbef
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-06 14:45:46 -0700

    Renamed objects to controlobjects microservice

--- checkout.ps1
+++ checkout.ps1
@@ -1,13 +1,13 @@
 #!/bin/pwsh
 
 git clone git@github.com:iquipsys-positron/iqs-services-devices-node
-git clone git@github.com:iquipsys-positron/iqs-services-objects-node
+git clone git@github.com:iquipsys-positron/iqs-services-controlobjects-node
 git clone git@github.com:iquipsys-positron/iqs-services-objectgroups-node
 git clone git@github.com:iquipsys-positron/iqs-services-dataprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-services-deviceprofiles-node
 
 git clone git@github.com:iquipsys-positron/iqs-clients-devices-node
-git clone git@github.com:iquipsys-positron/iqs-clients-objects-node
+git clone git@github.com:iquipsys-positron/iqs-clients-controlobjects-node
 git clone git@github.com:iquipsys-positron/iqs-clients-objectgroups-node
 git clone git@github.com:iquipsys-positron/iqs-clients-dataprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-clients-deviceprofiles-node
