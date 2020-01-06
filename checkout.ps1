a15016fe4e33b3118c3b52fa68c548994680eabc
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-06 15:27:28 -0700

    Added resolutions microservice

--- checkout.ps1
+++ checkout.ps1
@@ -1,4 +1,4 @@
-#!/bin/pwsh
+#!/usr/local/bin/pwsh
 
 git clone git@github.com:iquipsys-positron/iqs-services-devices-node
 git clone git@github.com:iquipsys-positron/iqs-services-controlobjects-node
@@ -7,6 +7,7 @@ git clone git@github.com:iquipsys-positron/iqs-services-dataprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-services-deviceprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-services-zones-node
 git clone git@github.com:iquipsys-positron/iqs-services-eventrules-node
+git clone git@github.com:iquipsys-positron/iqs-services-resolutions-node
 
 git clone git@github.com:iquipsys-positron/iqs-clients-devices-node
 git clone git@github.com:iquipsys-positron/iqs-clients-controlobjects-node
@@ -15,3 +16,4 @@ git clone git@github.com:iquipsys-positron/iqs-clients-dataprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-clients-deviceprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-clients-zones-node
 git clone git@github.com:iquipsys-positron/iqs-clients-eventrules-node
+git clone git@github.com:iquipsys-positron/iqs-clients-resoluitions-node
