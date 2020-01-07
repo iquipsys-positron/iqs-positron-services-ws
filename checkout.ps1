b8d25ebfa132e4e70d5b2f944a79d3d87ec25566
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-07 10:05:25 -0700

    Added protocols, device configs, shifts, emergency plans, gateways and locations

--- checkout.ps1
+++ checkout.ps1
@@ -1,5 +1,7 @@
 #!/usr/local/bin/pwsh
 
+git clone git@github.com:iquipsys-positron/iqs-libs-protocols-node
+
 git clone git@github.com:iquipsys-positron/iqs-services-devices-node
 git clone git@github.com:iquipsys-positron/iqs-services-controlobjects-node
 git clone git@github.com:iquipsys-positron/iqs-services-objectgroups-node
@@ -8,6 +10,11 @@ git clone git@github.com:iquipsys-positron/iqs-services-deviceprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-services-zones-node
 git clone git@github.com:iquipsys-positron/iqs-services-eventrules-node
 git clone git@github.com:iquipsys-positron/iqs-services-resolutions-node
+git clone git@github.com:iquipsys-positron/iqs-services-deviceconfigs-node
+git clone git@github.com:iquipsys-positron/iqs-services-shifts-node
+git clone git@github.com:iquipsys-positron/iqs-services-emergencyplans-node
+git clone git@github.com:iquipsys-positron/iqs-services-locations-node
+git clone git@github.com:iquipsys-positron/iqs-services-gateways-node
 
 git clone git@github.com:iquipsys-positron/iqs-clients-devices-node
 git clone git@github.com:iquipsys-positron/iqs-clients-controlobjects-node
@@ -17,3 +24,8 @@ git clone git@github.com:iquipsys-positron/iqs-clients-deviceprofiles-node
 git clone git@github.com:iquipsys-positron/iqs-clients-zones-node
 git clone git@github.com:iquipsys-positron/iqs-clients-eventrules-node
 git clone git@github.com:iquipsys-positron/iqs-clients-resolutions-node
+git clone git@github.com:iquipsys-positron/iqs-clients-deviceconfigs-node
+git clone git@github.com:iquipsys-positron/iqs-clients-shifts-node
+git clone git@github.com:iquipsys-positron/iqs-clients-emergencyplans-node
+git clone git@github.com:iquipsys-positron/iqs-clients-locations-node
+git clone git@github.com:iquipsys-positron/iqs-clients-gateways-node
